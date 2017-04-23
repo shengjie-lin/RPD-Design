e6cfcea7d6e9200949ce10914f66bc56a29935ff
Shengjie Lin <StArchon@outlook.com>
2017-04-23 21:53:58 +0800

    added translation files

---
+++ RpdDesign/rpddesign_en.ts
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>RpdDesign</name>
+    <message>
+        <location filename="RpdDesign.cpp" line="25"/>
+        <source>RPD Design</source>
+        <translation>RPD Design</translation>
+    </message>
+    <message>
+        <location filename="RpdDesign.cpp" line="26"/>
+        <source>Remedy</source>
+        <translation>Remedy</translation>
+    </message>
+    <message>
+        <location filename="RpdDesign.cpp" line="27"/>
+        <source>Base</source>
+        <translation>Base</translation>
+    </message>
+    <message>
+        <location filename="RpdDesign.cpp" line="28"/>
+        <source>Design</source>
+        <translation>Design</translation>
+    </message>
+    <message>
+        <location filename="RpdDesign.cpp" line="29"/>
+        <source>Switch Language</source>
+        <translation>中文</translation>
+    </message>
+    <message>
+        <location filename="RpdDesign.cpp" line="30"/>
+        <source>Load Base</source>
+        <translation>Load Base</translation>
+    </message>
+    <message>
+        <location filename="RpdDesign.cpp" line="31"/>
+        <source>Load RPD</source>
+        <translation>Load RPD</translation>
+    </message>
+    <message>
+        <location filename="RpdDesign.cpp" line="32"/>
+        <source>Save Design</source>
+        <translation>Save Design</translation>
+    </message>
+</context>
+<context>
+    <name>RpdViewer</name>
+    <message>
+        <location filename="RpdViewer.cpp" line="130"/>
+        <source>Select Base Image</source>
+        <translation>Select Base Image</translation>
+    </message>
+    <message>
+        <location filename="RpdViewer.cpp" line="130"/>
+        <source>All supported formats (*.bmp *.dib *.jpeg *.jpg *.jpe *.jp2 *.png *.pbm *.pgm *.ppm *.sr *.ras *.tiff *.tif);;Windows bitmaps (*.bmp *.dib);;JPEG files (*.jpeg *.jpg *.jpe);;JPEG 2000 files (*.jp2);;Portable Network Graphics (*.png);;Portable image format (*.pbm *.pgm *.ppm);;Sun rasters (*.sr *.ras);;TIFF files (*.tiff *.tif)</source>
+        <translation>All supported formats (*.bmp *.dib *.jpeg *.jpg *.jpe *.jp2 *.png *.pbm *.pgm *.ppm *.sr *.ras *.tiff *.tif);;Windows bitmaps (*.bmp *.dib);;JPEG files (*.jpeg *.jpg *.jpe);;JPEG 2000 files (*.jp2);;Portable Network Graphics (*.png);;Portable image format (*.pbm *.pgm *.ppm);;Sun rasters (*.sr *.ras);;TIFF files (*.tiff *.tif)</translation>
+    </message>
+    <message>
+        <location filename="RpdViewer.cpp" line="134"/>
+        <location filename="RpdViewer.cpp" line="419"/>
+        <location filename="RpdViewer.cpp" line="431"/>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <location filename="RpdViewer.cpp" line="134"/>
+        <source>Not a Valid Image!</source>
+        <translation>Not a Valid Image!</translation>
+    </message>
+    <message>
+        <location filename="RpdViewer.cpp" line="256"/>
+        <source>Select RPD Information</source>
+        <translation>Select RPD Information</translation>
+    </message>
+    <message>
+        <location filename="RpdViewer.cpp" line="256"/>
+        <source>Ontology files (*.owl)</source>
+        <translation>Ontology files (*.owl)</translation>
+    </message>
+    <message>
+        <location filename="RpdViewer.cpp" line="419"/>
+        <source>Not a Valid Ontology!</source>
+        <translation>Not a Valid Ontology!</translation>
+    </message>
+    <message>
+        <location filename="RpdViewer.cpp" line="425"/>
+        <source>Portable Network Graphics (*.png)</source>
+        <translation>Portable Network Graphics (*.png)</translation>
+    </message>
+    <message>
+        <location filename="RpdViewer.cpp" line="426"/>
+        <source>Select Save Path</source>
+        <translation>Select Save Path</translation>
+    </message>
+    <message>
+        <location filename="RpdViewer.cpp" line="426"/>
+        <source>Windows bitmaps (*.bmp *.dib);;JPEG files (*.jpeg *.jpg *.jpe);;JPEG 2000 files (*.jp2);;Portable Network Graphics (*.png);;Portable image format (*.pbm *.pgm *.ppm);;Sun rasters (*.sr *.ras);;TIFF files (*.tiff *.tif)</source>
+        <translation>Windows bitmaps (*.bmp *.dib);;JPEG files (*.jpeg *.jpg *.jpe);;JPEG 2000 files (*.jp2);;Portable Network Graphics (*.png);;Portable image format (*.pbm *.pgm *.ppm);;Sun rasters (*.sr *.ras);;TIFF files (*.tiff *.tif)</translation>
+    </message>
+    <message>
+        <location filename="RpdViewer.cpp" line="431"/>
+        <source>No Available Design!</source>
+        <translation>No Available Design!</translation>
+    </message>
+</context>
+</TS>
---
+++ RpdDesign/rpddesign_zh.ts
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>RpdDesign</name>
+    <message>
+        <location filename="RpdDesign.cpp" line="25"/>
+        <source>RPD Design</source>
+        <translation>RPD设计</translation>
+    </message>
+    <message>
+        <location filename="RpdDesign.cpp" line="26"/>
+        <source>Remedy</source>
+        <translation>矫正</translation>
+    </message>
+    <message>
+        <location filename="RpdDesign.cpp" line="27"/>
+        <source>Base</source>
+        <translation>底图</translation>
+    </message>
+    <message>
+        <location filename="RpdDesign.cpp" line="28"/>
+        <source>Design</source>
+        <translation>设计图</translation>
+    </message>
+    <message>
+        <location filename="RpdDesign.cpp" line="29"/>
+        <source>Switch Language</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <location filename="RpdDesign.cpp" line="30"/>
+        <source>Load Base</source>
+        <translation>加载底图</translation>
+    </message>
+    <message>
+        <location filename="RpdDesign.cpp" line="31"/>
+        <source>Load RPD</source>
+        <translation>加载RPD</translation>
+    </message>
+    <message>
+        <location filename="RpdDesign.cpp" line="32"/>
+        <source>Save Design</source>
+        <translation>保存设计图</translation>
+    </message>
+</context>
+<context>
+    <name>RpdViewer</name>
+    <message>
+        <location filename="RpdViewer.cpp" line="130"/>
+        <source>Select Base Image</source>
+        <translation>选择底图</translation>
+    </message>
+    <message>
+        <location filename="RpdViewer.cpp" line="130"/>
+        <source>All supported formats (*.bmp *.dib *.jpeg *.jpg *.jpe *.jp2 *.png *.pbm *.pgm *.ppm *.sr *.ras *.tiff *.tif);;Windows bitmaps (*.bmp *.dib);;JPEG files (*.jpeg *.jpg *.jpe);;JPEG 2000 files (*.jp2);;Portable Network Graphics (*.png);;Portable image format (*.pbm *.pgm *.ppm);;Sun rasters (*.sr *.ras);;TIFF files (*.tiff *.tif)</source>
+        <translation>所有支持的格式 (*.bmp *.dib *.jpeg *.jpg *.jpe *.jp2 *.png *.pbm *.pgm *.ppm *.sr *.ras *.tiff *.tif);;Windows位图 (*.bmp *.dib);;JPEG文件 (*.jpeg *.jpg *.jpe);;JPEG 2000文件 (*.jp2);;便携式网络图形 (*.png);;便携式图像格式 (*.pbm *.pgm *.ppm);;Sun栅格图 (*.sr *.ras);;TIFF文件 (*.tiff *.tif)</translation>
+    </message>
+    <message>
+        <location filename="RpdViewer.cpp" line="134"/>
+        <location filename="RpdViewer.cpp" line="419"/>
+        <location filename="RpdViewer.cpp" line="431"/>
+        <source>Error</source>
+        <translation>错误</translation>
+    </message>
+    <message>
+        <location filename="RpdViewer.cpp" line="134"/>
+        <source>Not a Valid Image!</source>
+        <translation>非有效的图像文件！</translation>
+    </message>
+    <message>
+        <location filename="RpdViewer.cpp" line="256"/>
+        <source>Select RPD Information</source>
+        <translation>选择RPD信息</translation>
+    </message>
+    <message>
+        <location filename="RpdViewer.cpp" line="256"/>
+        <source>Ontology files (*.owl)</source>
+        <translation>本体文件 (*.owl)</translation>
+    </message>
+    <message>
+        <location filename="RpdViewer.cpp" line="419"/>
+        <source>Not a Valid Ontology!</source>
+        <translation>非有效的本体文件！</translation>
+    </message>
+    <message>
+        <location filename="RpdViewer.cpp" line="425"/>
+        <source>Portable Network Graphics (*.png)</source>
+        <translation>便携式网络图形 (*.png)</translation>
+    </message>
+    <message>
+        <location filename="RpdViewer.cpp" line="426"/>
+        <source>Select Save Path</source>
+        <translation>选择存储路径</translation>
+    </message>
+    <message>
+        <location filename="RpdViewer.cpp" line="426"/>
+        <source>Windows bitmaps (*.bmp *.dib);;JPEG files (*.jpeg *.jpg *.jpe);;JPEG 2000 files (*.jp2);;Portable Network Graphics (*.png);;Portable image format (*.pbm *.pgm *.ppm);;Sun rasters (*.sr *.ras);;TIFF files (*.tiff *.tif)</source>
+        <translation>Windows位图 (*.bmp *.dib);;JPEG文件 (*.jpeg *.jpg *.jpe);;JPEG 2000文件 (*.jp2);;便携式网络图形 (*.png);;便携式图像格式 (*.pbm *.pgm *.ppm);;Sun栅格图 (*.sr *.ras);;TIFF文件 (*.tiff *.tif)</translation>
+    </message>
+    <message>
+        <location filename="RpdViewer.cpp" line="431"/>
+        <source>No Available Design!</source>
+        <translation>无可用的设计图！</translation>
+    </message>
+</context>
+</TS>
